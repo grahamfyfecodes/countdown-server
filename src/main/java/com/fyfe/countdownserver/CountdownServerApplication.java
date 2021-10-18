74a5a9f4306934c88e5162bb874e61e58991ef2d
grahamfyfecodes <67826423+grahamfyfecodes@users.noreply.github.com>
2021-10-18 13:26:57 +0100

    Init Commit

---
+++ src/main/java/com/fyfe/countdownserver/CountdownServerApplication.java
@@ -0,0 +1,13 @@
+package com.fyfe.countdownserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountdownServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountdownServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyfe/countdownserver/CountdownServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyfe.countdownserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountdownServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
